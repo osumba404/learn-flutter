9d2831d1e6ebc774f7c304bc540ab5a40a16eed5
Evans Osumba <osumbaevanzz@gmail.com>
2025-06-30 16:07:29 +0300

    currency converter app first code

--- currency_converter/lib/currency_converter_material_page.dart
+++ currency_converter/lib/currency_converter_material_page.dart
@@ -26,53 +26,56 @@ class CurrencyConverterMaterialPage extends StatelessWidget{
         mainAxisAlignment: MainAxisAlignment.center, 
        
         children: [
-          Text("Debug!", 
+          Text("Debug Mode!", 
           style: TextStyle(
             fontSize: 30,
             fontWeight: FontWeight.w700,
             color: Color.fromARGB(255, 0,  0, 255),
           ),),
 
-            TextField(
-              style: TextStyle(
-                color: Colors.white
-              ),
-              decoration: InputDecoration(
-                //   helper: Text("Enter amount in KES", style: TextStyle(
-                //   color: Color.fromARGB(255, 0,  0, 255),
-                // )
-                hintText: "Enter amount in KES",
-                hintStyle: TextStyle(
-                  color: Colors.white70,
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.white
                 ),
-                prefixIcon: Icon((Icons.monetization_on_outlined)), 
-                prefixIconColor: Colors.white70,  
-
-                // suffixIcon: Icon((Icons.monetization_on_outlined)), 
-                // suffixIconColor: Colors.white70, 
-                filled: true,
-                fillColor: Color.fromRGBO(0, 170, 255, 1),   
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                     color: Color.fromARGB(255, 41, 19, 19),
-                     width: 2,
-                     style: BorderStyle.solid,
-                     strokeAlign:BorderSide.strokeAlignCenter,                     
+                decoration: InputDecoration(
+                  //   helper: Text("Enter amount in KES", style: TextStyle(
+                  //   color: Color.fromARGB(255, 0,  0, 255),
+                  // )
+                  hintText: "Enter amount in KES",
+                  hintStyle: TextStyle(
+                    color: Colors.white70,
                   ),
-                  borderRadius: BorderRadius.all(Radius.circular(30)),
-                ),
-                enabledBorder: OutlineInputBorder(                
-                  borderSide: BorderSide(
-                     color: Color.fromARGB(255, 41, 19, 19),
-                     width: 2,
-                     style: BorderStyle.solid,
-                     strokeAlign:BorderSide.strokeAlignCenter,                     
+                  prefixIcon: Icon((Icons.monetization_on_outlined)), 
+                  prefixIconColor: Colors.white70,  
+              
+                  // suffixIcon: Icon((Icons.monetization_on_outlined)), 
+                  // suffixIconColor: Colors.white70, 
+                  filled: true,
+                  fillColor: Color.fromRGBO(0, 170, 255, 1),   
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                       color: Color.fromARGB(255, 41, 19, 19),
+                       width: 2,
+                       style: BorderStyle.solid,
+                       strokeAlign:BorderSide.strokeAlignCenter,                     
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
                   ),
-                  borderRadius: BorderRadius.all(Radius.circular(30)),
-                
+                  enabledBorder: OutlineInputBorder(                
+                    borderSide: BorderSide(
+                       color: Color.fromARGB(255, 41, 19, 19),
+                       width: 2,
+                       style: BorderStyle.solid,
+                       strokeAlign:BorderSide.strokeAlignCenter,                     
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                  
+                  ),
                 ),
+                keyboardType: TextInputType.number,
               ),
-              keyboardType: TextInputType.number,
             ),
         ],
         )) 
